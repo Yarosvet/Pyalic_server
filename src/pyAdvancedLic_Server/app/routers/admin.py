9fbbf80f3ec91a2e11b49486190b64a156a57c70
Yarosvet <yarosvetk@yandex.ru>
2023-10-21 06:02:19 +0300

    Signatures method fixed

--- src/pyAdvancedLic_Server/app/routers/admin.py
+++ src/pyAdvancedLic_Server/app/routers/admin.py
@@ -171,7 +171,7 @@ async def get_signature(payload: schema.IdField,
                         current_user: schema.User = Depends(auth.get_current_user)):
     r = await session.execute(
         select(models.Signature).filter_by(id=payload.id).options(
-            selectinload(models.Signature.installations).selectinload(models.Signature.product)))
+            selectinload(models.Signature.installations), selectinload(models.Signature.product)))
     sig = r.scalar_one_or_none()
     user_in_db = await _get_user_with_prod(current_user, session)
     if not user_in_db.get_verifiable_permissions().able_get_product(sig.product):
@@ -217,7 +217,7 @@ async def update_signature(payload: schema.UpdateSignature,
                            current_user: schema.User = Depends(auth.get_current_user)):
     r = await session.execute(
         select(models.Signature).filter_by(id=payload.id).options(
-            selectinload(models.Signature.installations).selectinload(models.Signature.product)))
+            selectinload(models.Signature.installations), selectinload(models.Signature.product)))
     sig = r.scalar_one_or_none()
     if sig is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Signature not found")
