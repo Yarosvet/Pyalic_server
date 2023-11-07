0db6497be4a07077f3898517c9d278f72b171493
Yarosvet <yarosvetk@yandex.ru>
2023-11-07 09:51:47 +0300

    Tests environment

--- src/tests/server/__init__.py
+++ src/tests/server/__init__.py
@@ -1,20 +1,28 @@
 from sqlalchemy import create_engine
-import sqlalchemy.orm as orm
 import random
 import string
+from sqlalchemy import orm
+from sqlalchemy.ext.serializer import dumps, loads
 
 from src.pyAdvancedLic_Server.app import config, db
 
 conn_str = f'postgresql://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}/{config.DB_NAME}'
 engine = create_engine(conn_str, echo=False)
 __factory = orm.sessionmaker(bind=engine, expire_on_commit=False)
-from src.pyAdvancedLic_Server.app.db import models  # noqa
+db.SqlAlchemyBase.metadata.drop_all(bind=engine)
+
+db_dump = None
+
+
+def save_db_state():
+    global db_dump
+    with __factory() as session:
+        db_dump = dumps(session)
+
+
+def load_db_state():
+    loads(db_dump, scoped_session=orm.scoped_session(__factory))
 
 
 def rand_str(length: int) -> str:
     return "".join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])
-
-
-def rebuild_db():
-    db.SqlAlchemyBase.metadata.drop_all(bind=engine)
-    db.SqlAlchemyBase.metadata.create_all(bind=engine)
---
+++ src/tests/server/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+from fastapi.testclient import TestClient
+import time
+
+from src.pyAdvancedLic_Server.app import config, app
+
+from . import load_db_state, save_db_state
+
+
+@pytest.fixture(scope="session")
+def client():
+    with TestClient(app) as c:
+        time.sleep(1)
+        save_db_state()
+        yield c
+
+
+@pytest.fixture(scope="function")
+def rebuild_db():
+    load_db_state()
+
+
+@pytest.fixture(scope="class")
+def auth(client):
+    p = {
+        "grant_type": "password",
+        "username": config.DEFAULT_USER,
+        "password": config.DEFAULT_PASSWORD
+    }
+    r = client.request('POST', '/admin/token', data=p)
+    token = r.json()['access_token']
+    yield {'Authorization': f"Bearer {token}"}
