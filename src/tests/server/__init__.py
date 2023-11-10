e43a9bf66b9f4b6ced38827f1c86d6663cdaa7de
Yarosvet <yarosvetk@yandex.ru>
2023-11-10 15:52:53 +0300

    Dumping and rebuilding DB fixed

--- src/tests/server/__init__.py
+++ src/tests/server/__init__.py
@@ -9,9 +9,9 @@ from src.pyAdvancedLic_Server.app import config, db
 conn_str = f'postgresql://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}/{config.DB_NAME}'
 engine = create_engine(conn_str, echo=False)
 __factory = orm.sessionmaker(bind=engine, expire_on_commit=False)
-db.SqlAlchemyBase.metadata.drop_all(bind=engine)
 
-db_dump = None
+db_dump_tables = set()
+db_dump_sequences = {}
 
 
 def create_db_session() -> orm.Session:
@@ -19,27 +19,39 @@ def create_db_session() -> orm.Session:
 
 
 def save_db_state():
-    global db_dump
+    global db_dump_tables
+    global db_dump_sequences
     with create_db_session() as session:
-        db_dump = dumps(session)
+        for mapper in db.SqlAlchemyBase.registry.mappers:
+            db_dump_tables.add(dumps(session.query(mapper.class_).all()))
+        q = (i[0] for i in session.execute("SELECT sequence_name FROM information_schema.sequences;").all())
+        for seq_name in q:
+            current_val = session.execute(f"SELECT nextval('{seq_name}')-1;").scalar()
+            if current_val != 0:
+                db_dump_sequences[seq_name] = current_val
 
 
 def load_db_state():
-    loads(db_dump, scoped_session=orm.scoped_session(__factory))
+    with create_db_session() as session:
+        for table_class in db_dump_tables:
+            restored = loads(table_class, scoped_session=orm.scoped_session(__factory))
+            for obj in restored:
+                session.merge(obj)
+            session.commit()
+        q = (i[0] for i in session.execute("SELECT sequence_name FROM information_schema.sequences;").all())
+        for seq_name in q:
+            if seq_name in db_dump_sequences.keys():
+                current_value = db_dump_sequences[seq_name]
+                session.execute(f"SELECT setval('{seq_name}', {current_value});")
 
 
 def clean_db():
     db.SqlAlchemyBase.metadata.drop_all(bind=engine)
 
 
+def fill_db():
+    db.SqlAlchemyBase.metadata.create_all(bind=engine)
+
+
 def rand_str(length: int) -> str:
     return "".join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])
-
-
-def create_rand_user(permissions: str, master_username: str = None) -> tuple[int, str, str]:
-    """
-    :param master_username: Master's username
-    :param permissions: User's permissions string
-    :return: username and password
-    """
-
--- src/tests/server/conftest.py
+++ src/tests/server/conftest.py
@@ -4,11 +4,11 @@ import time
 
 from src.pyAdvancedLic_Server.app import config, app
 
-from . import load_db_state, save_db_state, clean_db
+from . import load_db_state, save_db_state, clean_db, fill_db
 
 
 @pytest.fixture(scope="session")
-def client():
+def client() -> TestClient:
     with TestClient(app) as c:
         time.sleep(1)
         save_db_state()
@@ -17,17 +17,20 @@ def client():
 
 @pytest.fixture(scope="function")
 def rebuild_db():
+    clean_db()
+    fill_db()
     load_db_state()
 
 
 @pytest.fixture(scope="session", autouse=True)
-def clean_db_on_finish():
+def clean_db_on_start_and_finish():
+    clean_db()
     yield
     clean_db()
 
 
 @pytest.fixture(scope="class")
-def auth(client):
+def auth(client) -> dict[str, str]:
     p = {
         "grant_type": "password",
         "username": config.DEFAULT_USER,
