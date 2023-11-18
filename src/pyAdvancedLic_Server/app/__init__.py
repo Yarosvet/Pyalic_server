cdd63afe175df2b70232cb53c5a3c9298df09636
Yarosvet <yarosvetk@yandex.ru>
2023-11-18 18:07:50 +0300

    Lifespan events instead of deprecated alternative events

--- src/pyAdvancedLic_Server/app/__init__.py
+++ src/pyAdvancedLic_Server/app/__init__.py
@@ -2,6 +2,7 @@
 Python Advanced Licensing System server
 (Main Fastapi App configuration module)
 """
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
 
@@ -10,21 +11,22 @@ from .licensing import sessions as lic_sessions
 from . import loggers, db, config
 from .access import create_default_user_if_not_exists
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(application: FastAPI):  # pylint: disable=unused-argument
+    """Lifespan of FastAPI application"""
+    await db.global_init(config.DB_USER, config.DB_PASSWORD, config.DB_HOST, config.DB_NAME)
+    await create_default_user_if_not_exists()
+    await periodic_clean_expired_sessions()  # Call in once, then it will be calling by itself
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(admin.router, prefix='/admin')
 app.include_router(admin.public_router, prefix='/admin')
 app.include_router(user.router)
 
 
-# Todo: on_event is deprecated, use lifespan event handlers instead
-@app.on_event('startup')
-async def init_models_db():
-    """Init database when app starts"""
-    await db.global_init(config.DB_USER, config.DB_PASSWORD, config.DB_HOST, config.DB_NAME)
-    await create_default_user_if_not_exists()
-
-
-@app.on_event('startup')
 @repeat_every(seconds=2)
 async def periodic_clean_expired_sessions():
     """Clean expired sessions when time is it"""
