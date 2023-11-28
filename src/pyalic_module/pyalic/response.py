1bc089bd061b684fc98aa2dbcdc8fbc027f3ae3c
Yarosvet <yarosvetk@yandex.ru>
2023-11-28 16:03:12 +0300

    Enforcing types for dataclass fields

--- src/pyalic_module/pyalic/response.py
+++ src/pyalic_module/pyalic/response.py
@@ -1,5 +1,6 @@
 """Responses environment and managing responses"""
 from dataclasses import dataclass
+from collections import ChainMap
 
 
 @dataclass
@@ -9,6 +10,16 @@ class OperationResponse:
     success: bool
     content: dict
     error: str = None
+
+    @classmethod
+    def _get_annotations(cls):
+        """Get annotations from all parent classes including current class"""
+        return ChainMap(*(c.__annotations__ for c in cls.__mro__ if '__annotations__' in c.__dict__))
+
+    def __post_init__(self):
+        for (name, field_type) in self.__class__._get_annotations().items():  # pylint: disable=protected-access
+            if not isinstance(getattr(self, name), field_type) and getattr(self, name) is not None:
+                setattr(self, name, field_type(getattr(self, name)))
 
 
 @dataclass
