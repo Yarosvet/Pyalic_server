60c8b480c0771f499552efe8b38bf26a40d28e08
Yarosvet <yarosvetk@yandex.ru>
2023-12-02 15:49:35 +0300

    Setup configuration

--- src/pyalic_module/setup.py
+++ src/pyalic_module/setup.py
@@ -2,6 +2,9 @@
 import os
 from setuptools import setup, find_packages
 
+with open("../../README.md", "r", encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
 
 def requirements():
     """Get current requirements"""
@@ -12,6 +15,18 @@ def requirements():
 setup(
     name='pyalic',
     version='1.0.0',
+    author='Yarosvet',
+    description="Licensing system module which allows you to manage access to your products with ease",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    url="https://github.com/Yarosvet/Pyalic",
     packages=find_packages(include=['pyalic', 'pyalic.*']),
-    install_requires=[*requirements()]
+    install_requires=[*requirements()],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
